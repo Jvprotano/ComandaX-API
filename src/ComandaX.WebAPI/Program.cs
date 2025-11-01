1faa7c8da7e45e802e6742ece2e0f97343abd4c9
José Vinícius <jvprotano@gmail.com>
2025-11-01 00:28:53 -0300

    Added update migration when running

--- src/ComandaX.WebAPI/Program.cs
+++ src/ComandaX.WebAPI/Program.cs
@@ -37,4 +37,8 @@ app.UseRouting();
 
 app.MapGet("/", () => "API running ✅");
 
+using var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+db.Database.Migrate();
+
 app.Run();
